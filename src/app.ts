627f3cbb4569e4a6a8ff56d0ef8d795be0fe5fa9
BASANT SONI <basant@LAPTOP-BA8VSS9D.localdomain>
2022-09-16 13:35:00 +0530

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 import express, { Express, NextFunction, Response } from 'express'
 import createError from 'http-errors'
 import logger from 'morgan'
@@ -10,6 +11,7 @@ const app: Express = express()
 
 app.use(logger('dev'))
 app.use(express.json())
+app.use(cors())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
